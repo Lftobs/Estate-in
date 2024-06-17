78bc77fc69f3c073e2a5a4479d16af57cb493318
lftobs <tobbytobs1@gmail.com>
2024-06-17 21:38:59 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,14 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "background-black": "rgba(38, 38, 38, 1)",
+        "purple": "rgba(112, 59, 247, 1)"
       },
+      backgroundColor: {
+        "background-black": "rgba(38, 38, 38, 1)",
+        "purple": "rgba(112, 59, 247, 1)",
+        "grey": "rgba(153, 153, 153, 1)"
+      }
     },
   },
   plugins: [],
