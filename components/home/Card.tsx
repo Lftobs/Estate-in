02b30faa3291fdfb822f5dd6fbb902f32505f2aa
lftobs <tobbytobs1@gmail.com>
2024-06-29 23:20:39 +0100

    added docker

--- components/home/Card.tsx
+++ components/home/Card.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image"
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <>
+      <div className=" bg-hero-img-bg p-5 flex relative flex-col w-auto items-center ring-1 ring-border-black rounded-xl">
+        <Image src="/card-img1.svg" alt="card-img" width={80} height={80} />
+        <p className="mt-8 text-balance font-semibold text-center">Find Your Dream Home </p>
+        <Image src="/arrow.svg" alt="arrow" width={20} height={20}  className="absolute top-3 right-5"/>
+      </div>
+    </>
   )
 }
 
